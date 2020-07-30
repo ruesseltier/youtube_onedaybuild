c19010e11f9e35136de2e4c5e352ce33579295be
René Kurfürst <rkurfuerst@googlemail.com>
2020-07-30 17:20:47 +0200

    Initial Commit

---
+++ youtube_onedaybuild/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube_onedaybuild
+//
+//  Created by Rene Kurfürst on 30.07.20.
+//  Copyright © 2020 ruesseltier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
